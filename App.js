5d5a0ef046151c71ef78876f016c423918548f2c
TaeHongXi <taehongxi@student.usm.my>
2021-10-17 15:11:11 +0800

    hx testing

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Pocket</Text>
+      <Text>HX Pocket</Text>
       <StatusBar style="auto" />
     </View>
   );
