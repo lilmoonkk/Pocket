050336e9b3179b004646de071ab53daa6072ce5b
lilmoonkk <moonkoo5550@gmail.com>
2022-01-22 00:24:33 +0800

    UpdateGoal

---
+++ backend/routes/UpdateGoal.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const Router = express.Router();
+const mysqlConnection = require("../connection");
+
+
+Router.post('/', (req, res) => {
+    let {
+        id,
+        amount
+    } = req.body;
+ 
+    var data = {
+        id : id,
+        amount : amount
+    };
+   
+    mysqlConnection.query("UPDATE goal SET goal.amount = ? WHERE goal.id = ?;", 
+    [data.amount, data.id],
+       function(err, rows) {
+          if (err) {
+             //If error
+             res.status(400).json('Sorry!!Unable To Update');
+          console.log("Error updating : %s ", err);
+       } else{
+          //If success
+          res.status(200).json('Goal Updated Successfully!!');
+       }
+    });
+});
+
+module.exports = Router;
